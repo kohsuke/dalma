8a891a80db70575aa6cb8d8bba1061e5c07e4076
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-10-06 16:49:42 +0000

    added a test code for Spring

---
+++ modules/email/src/test/test/spring/SpringTest.java
@@ -0,0 +1,20 @@
+package test.spring;
+
+import dalma.EngineFactory;
+import dalma.Engine;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import junit.framework.TestCase;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class SpringTest extends TestCase {
+    public void test1() throws Exception {
+        XmlBeanFactory xbf = new XmlBeanFactory(new ClassPathResource("config.xml",SpringTest.class));
+        EngineFactory ef = (EngineFactory)xbf.getBean("engine");
+        Engine engine = ef.newInstance();
+        assertNotNull(engine.getEndPoint("e-mail"));
+        engine.stop();
+    }
+}
