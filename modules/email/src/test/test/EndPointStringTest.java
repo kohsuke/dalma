23dd271599ab37fd42c9eccd2de17d01933d1ee3
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-10-07 04:45:23 +0000

    modified so that the test works regardless of the current directory

--- modules/email/src/test/test/EndPointStringTest.java
+++ modules/email/src/test/test/EndPointStringTest.java
@@ -19,7 +19,7 @@ public class EndPointStringTest extends TestCase {
             new File("target/endpoint-string-test"),
             getClass().getClassLoader(),
             new ThreadPoolExecutor(3));
-        EndPoint ep = engine.addEndPoint("mail", "smtp://dalma-test1@kohsuke.org!maildir://./mail");
+        EndPoint ep = engine.addEndPoint("mail", "smtp://dalma-test1@kohsuke.org!maildir://.");
         assertTrue(ep instanceof EmailEndPoint);
         engine.stop();
     }
