dac3dd89ac464be6c852eb084ea47aef9e9c663a
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-02 06:49:44 +0000

    added entry point.

---
+++ modules/container/src/java/dalma/Program.java
@@ -0,0 +1,77 @@
+package dalma;
+
+/**
+ * Entry point of the user-implemented workflow application.
+ *
+ * <p>
+ * A workflow application that runs inside the dalma container
+ * needs to have a class that derives from this class. Such class
+ * serves as the entry point to the workflow program, and its fully-qualified
+ * class name must be <tt>Main</tt> (in the root package.)
+ *
+ * <h3>Configuring Program</h3>
+ * <p>
+ * A {@link Program} typically needs to be configured with environment-specific
+ * information to function. For example, perhaps a program is some kind of e-mail
+ * automation system and may use 2 e-mail endpoints and 1 IRC endpoint.
+ *
+ * A {@link Program} communicates these configuration requirements to the container
+ * by using {@link Resource} annotation. In the above example, the {@link Program}
+ * would be written something like:
+ *
+ * <pre>
+ * class Main extends dalma.Program {
+ *   &#x40;Resource
+ *   public EmailEndPoint externalMail;
+ *
+ *   &#x40;Resource
+ *   public EmailEndPoint maintananceMail;
+ *
+ *   &#x40;Resource
+ *   public IRCEndPoint chatEndPoint;
+ *
+ *   &#x40;Resource
+ *   public String greetingMessage;
+ *
+ *   ...
+ * }
+ * </pre>
+ *
+ * <p>
+ * At runtime, the container invokes setters and sets fields to "inject" references
+ * into the program before the {@link #init(Engine)} method
+ * is invoked.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class Program {
+    /**
+     * Called right after the {@link Engine} instance is created
+     * and populated with the configured endpoints.
+     *
+     * <p>
+     * In rare case, when a {@link Program} doesn't know statically
+     * what endpoints it wants to use,
+     * this callback can be used to add additional endpoints
+     * programatically if desired.
+     *
+     * <p>
+     * Note that if kinds and endpoints are known statically,
+     * then the {@link Program}-derived class can use a resource injection
+     * to get access to those endpoints.
+     */
+    public void init(Engine engine) {
+        // noop
+    }
+
+    /**
+     * Called after the {@link Engine} is {@link Engine#start()} started.
+     *
+     * <p>
+     * In rare case, when a {@link Program} wants to start a new
+     * {@link Conversation} proactively, it can use this callback to do so.
+     */
+    public void main() {
+        // noop
+    }
+}
---
+++ modules/container/src/java/dalma/Resource.java
@@ -0,0 +1,49 @@
+package dalma;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+/**
+ * Identifies a field/method that needs to be injected by the dalma container.
+ *
+ * <p>
+ * A resource must be one of the following types:
+ *
+ * <ol>
+ *  <li>{@link EndPoint}-derived type.
+ *      The container will require the user to configure an endpoint and injects the
+ *      configured endpoint.
+ *  <li>{@link String}
+ *      The container will inject a string that the user configured.
+ *  <li>{@code boolean}
+ *      The container will inject a true/false option that the user configured.
+ *  <li>{@code int}
+ *      The container will inject a number that the user configured.
+ * </ol>
+ *
+ * <p>
+ * ... and it must be either a public field of the form {@code public T xyz}
+ * or a public setter method of the form {@code public void setXyz(T)}.
+ *
+ * <p>
+ * The container uses these annotations to determine what configuration is needed
+ * by a program.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Retention(RUNTIME)
+@Target({FIELD, METHOD})
+public @interface Resource {
+    /**
+     * Human-readable message that explains what this resource is.
+     *
+     * This message is presented to the user in the configuration screen.
+     * For example, this could be something like
+     * "e-mail address that is connected to the daemon" (for e-mail endpoint)
+     * or maybe "the greeting message" (for a string resource.)
+     */
+    String description() default "";
+}
