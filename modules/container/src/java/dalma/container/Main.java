074ab3ace8eaef383134b71c0f4b89c48c5976c4
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-07 05:29:42 +0000

    experimented with the shut down hook but it doesn't work very well, especially with cygwin.

--- modules/container/src/java/dalma/container/Main.java
+++ modules/container/src/java/dalma/container/Main.java
@@ -3,12 +3,12 @@ package dalma.container;
 import dalma.helpers.Java5Executor;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.concurrent.Executors;
-import java.util.logging.Logger;
-import java.util.logging.LogManager;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Entry point to the dalma container.
@@ -18,14 +18,19 @@ import java.util.logging.Level;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File home = getHome();
 
         logger.info("Starting dalma container with DALMA_HOME="+home);
 
-        new Container(home,new Java5Executor(Executors.newFixedThreadPool(5)));
+        new Container(home, new Java5Executor(Executors.newFixedThreadPool(5)));
 
         Thread.currentThread().suspend();
+
+        //Thread.sleep(10*1000);
+        //logger.info("Terminating");
+        //container.stop();
+        //logger.info("done");
     }
 
     /**
