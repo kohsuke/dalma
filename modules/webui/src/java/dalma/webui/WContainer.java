a3fd8dafe1104ed46feae5e963ed884f01b76986
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-17 06:40:20 +0000

    implemented the core of the file upload logic.

--- modules/webui/src/java/dalma/webui/WContainer.java
+++ modules/webui/src/java/dalma/webui/WContainer.java
@@ -1,11 +1,15 @@
 package dalma.webui;
 
 import dalma.container.Container;
+import org.apache.commons.fileupload.DiskFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * {@link Container} wrapper for the web UI.
@@ -28,6 +32,36 @@ public class WContainer implements UIObject {
     }
 
     public void doCreateApp(StaplerRequest req, StaplerResponse resp ) throws IOException, ServletException {
+        String appName = null;
+        byte[] contents = null;
+
+        try {
+            DiskFileUpload fu = new DiskFileUpload();
+            for( FileItem fi : (List<FileItem>)fu.parseRequest(req) ) {
+                if(fi.isFormField()) {
+                    if(fi.getFieldName()!=null && fi.getFieldName().equals("name"))
+                        appName = fi.getString();
+                } else {
+                    contents = fi.get();
+                }
+            }
+        } catch (FileUploadException e) {
+            sendError(req, e.getMessage(), resp);
+            return;
+        }
+
+        if(appName==null || contents==null || appName.length()==0 || contents.length==0) {
+            sendError(req, "form data incomplete", resp);
+            return;
+        }
+
+        System.out.println(appName);
+
         resp.sendRedirect(req.getContextPath());
+    }
+
+    private void sendError(StaplerRequest req, String msg, StaplerResponse resp) throws ServletException, IOException {
+        req.setAttribute("message",msg);
+        resp.forward(this,"error",req);
     }
 }
