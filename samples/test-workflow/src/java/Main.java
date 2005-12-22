b52e48efee9c350f09f5764c35b486ad4d5b10bd
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-22 05:24:35 +0000

    added resource for testing.

--- samples/test-workflow/src/java/Main.java
+++ samples/test-workflow/src/java/Main.java
@@ -1,12 +1,18 @@
 import dalma.Program;
 import dalma.Engine;
 import dalma.Description;
+import dalma.Resource;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 @Description("this is a do-nothing workflow application")
 public class Main extends Program {
+    @Resource(description="description of resource X")
+    public int x;
+    @Resource(description="this is Y")
+    public String y;
+
     public void main(Engine engine) throws Exception {
         for( int i=0; i<10; i++ )
             System.out.println("Running");
