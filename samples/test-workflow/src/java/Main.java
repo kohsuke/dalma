c3fa01328a138c347d3d29e2ef222af74a68f816
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-22 04:42:20 +0000

    updated to use @Description annotation.

--- samples/test-workflow/src/java/Main.java
+++ samples/test-workflow/src/java/Main.java
@@ -1,16 +1,14 @@
 import dalma.Program;
 import dalma.Engine;
+import dalma.Description;
 
 /**
  * @author Kohsuke Kawaguchi
  */
+@Description("this is a do-nothing workflow application")
 public class Main extends Program {
     public void main(Engine engine) throws Exception {
         for( int i=0; i<10; i++ )
             System.out.println("Running");
-    }
-
-    public String getDescription() {
-        return "this is a do-nothing workflow application";
     }
 }
