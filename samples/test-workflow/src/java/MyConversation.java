b550def260508b272d0730f00dea553fa1f2f815
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-25 15:02:39 +0000

    modified to report a title for testing.

--- samples/test-workflow/src/java/MyConversation.java
+++ samples/test-workflow/src/java/MyConversation.java
@@ -1,19 +1,21 @@
 import dalma.Engine;
 import static dalma.TimeUnit.SECONDS;
+import dalma.Workflow;
 import dalma.endpoints.timer.TimerEndPoint;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.Random;
 
 /**
  * @author Kohsuke Kawaguchi
  */
-public class MyConversation implements Runnable, Serializable {
-    int i = iota++;
+public class MyConversation extends Workflow {
+
+    int id = iota++;
     Random r = new Random();
     public void run() {
         for( int i=0; i<5; i++ ) {
+            setTitle(String.format("ID %1$s in its %2$s life",id,i));
             System.out.print('.');
             switch(r.nextInt(3)) {
             case 0:
