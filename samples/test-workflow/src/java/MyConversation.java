6a10c5d09845ea6fc42490b5b0dac7b00c32ba1b
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-26 01:39:12 +0000

    modified to use logger.

--- samples/test-workflow/src/java/MyConversation.java
+++ samples/test-workflow/src/java/MyConversation.java
@@ -1,7 +1,5 @@
-import dalma.Engine;
 import static dalma.TimeUnit.SECONDS;
 import dalma.Workflow;
-import dalma.endpoints.timer.TimerEndPoint;
 
 import java.io.IOException;
 import java.util.Random;
@@ -17,9 +15,11 @@ public class MyConversation extends Workflow {
         for( int i=0; i<5; i++ ) {
             setTitle(String.format("ID %1$s in its %2$s life",id,i));
             System.out.print('.');
+            getLogger().fine("Rolling a dice");
             switch(r.nextInt(3)) {
             case 0:
                 // reproduce
+                getLogger().fine("reproduce");
                 try {
                     getOwner().getEngine().createConversation(new MyConversation());
                 } catch (IOException e) {
@@ -29,9 +29,11 @@ public class MyConversation extends Workflow {
                 break;
             case 1:
                 // noop
+                getLogger().fine("noop");
                 break;
             case 2:
                 // die
+                getLogger().fine("die");
                 return;
             }
             sleep(3, SECONDS);
