fbfa7229129b2f4a2e375b637537b9eae04cf0b0
Kohsuke Kawaguchi <kk@kohsuke.org>
2006-01-04 06:56:12 +0000

    since webui requires Tiger, no need to use ant.

--- modules/webui/src/java/dalma/webui/EnvVars.java
+++ modules/webui/src/java/dalma/webui/EnvVars.java
@@ -1,11 +1,6 @@
 package dalma.webui;
 
-import org.apache.tools.ant.taskdefs.Execute;
-
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 /**
  * Access to the environmental variables.
@@ -16,16 +11,5 @@ class EnvVars {
     /**
      * Environmental variables that we've inherited.
      */
-    public static final Map<String,String> masterEnvVars;
-
-    static {
-        Vector<String> envs = Execute.getProcEnvironment();
-        Map<String,String> m = new HashMap<String,String>();
-        for (String e : envs) {
-            int idx = e.indexOf('=');
-            m.put(e.substring(0, idx), e.substring(idx + 1));
-        }
-        masterEnvVars = Collections.unmodifiableMap(m);
-    }
-
+    public static final Map<String,String> masterEnvVars = System.getenv();
 }
